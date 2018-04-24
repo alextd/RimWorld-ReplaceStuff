b6189c1f131a489409dea64603d17aa7d2dfb2cc
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2018-04-24 03:09:00 -0700

    Initial blank project

---
+++ Source/Replace_Stuff.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using Verse;
+using UnityEngine;
+using Harmony;
+
+namespace Replace_Stuff
+{
+	public class Mod : Verse.Mod
+	{
+		public Mod(ModContentPack content) : base(content)
+		{
+			// initialize settings
+			// GetSettings<Settings>();
+#if DEBUG
+			HarmonyInstance.DEBUG = true;
+#endif
+			HarmonyInstance harmony = HarmonyInstance.Create("Uuugggg.rimworld.Replace_Stuff.main");
+			harmony.PatchAll(Assembly.GetExecutingAssembly());
+		}
+
+//		public override void DoSettingsWindowContents(Rect inRect)
+//		{
+//			base.DoSettingsWindowContents(inRect);
+//			GetSettings<Settings>().DoWindowContents(inRect);
+//		}
+//
+//		public override string SettingsCategory()
+//		{
+//			return "Replace Stuff";
+//		}
+	}
+}
---
+++ Source/Settings.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+using RimWorld;
+
+namespace Replace_Stuff
+{
+	class Settings : ModSettings
+	{
+		public bool setting;
+
+		public static Settings Get()
+		{
+			return LoadedModManager.GetMod<Replace_Stuff.Mod>().GetSettings<Settings>();
+		}
+
+		public void DoWindowContents(Rect wrect)
+		{
+			var options = new Listing_Standard();
+			options.Begin(wrect);
+			
+			options.CheckboxLabeled("Sample setting", ref setting);
+			options.Gap();
+
+			options.End();
+		}
+		
+		public override void ExposeData()
+		{
+			Scribe_Values.Look(ref setting, "setting", true);
+		}
+	}
+}
