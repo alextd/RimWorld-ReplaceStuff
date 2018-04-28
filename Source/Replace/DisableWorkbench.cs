9386f54f8c9500e1e29ed2036d87be2ab698cdac
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2018-04-27 21:05:56 -0700

    Disable workbenches while it is being replaced

---
+++ Source/Replace/DisableWorkbench.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Harmony;
+using Verse;
+using RimWorld;
+
+namespace Replace_Stuff.Replace
+{
+	[HarmonyPatch(typeof(Building_WorkTable), "get_UsableNow")]
+	class DisableWorkbench
+	{
+		//public virtual bool UsableNow
+		public static void Postfix(ref bool __result, Building_WorkTable __instance)
+		{
+			if (__instance.Position.GetThingList(__instance.Map).FirstOrDefault(t => t is ReplaceFrame) is ReplaceFrame frame
+				&& frame.workDone > 0)
+				__result = false;
+		}
+	}
+}
