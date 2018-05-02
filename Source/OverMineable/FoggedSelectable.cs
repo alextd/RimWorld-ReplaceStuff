b7666ae08429c0f0a1ecf2dc84f9e1f26fa41a98
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2018-05-01 21:42:22 -0700

    Blueprints are selectable even in fog.

---
+++ Source/OverMineable/FoggedSelectable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Harmony;
+using RimWorld;
+using Verse;
+
+namespace Replace_Stuff.OverMineable
+{
+	[HarmonyPatch(typeof(ThingSelectionUtility), "SelectableByMapClick")]
+	class FoggedSelectable
+	{
+		//public static bool SelectableByMapClick(Thing t)
+		public static bool Prefix(ref bool __result, Thing t)
+		{
+			if (t.def.IsBlueprint) // && t.def.selectable && t.Spawned //redundant checks
+			{
+				__result = true;
+				return false;
+			}
+			return true;
+		}
+	}
+}
