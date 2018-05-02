37fddeed574d561f85c72505ea2eb3311305e6ac
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2018-05-02 06:11:03 -0700

    Pawns don't block construction.

---
+++ Source/Other/PawnBlockConstruction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Harmony;
+using RimWorld;
+using Verse;
+
+namespace Replace_Stuff.Other
+{
+	[HarmonyPatch(typeof(GenConstruct), "BlocksConstruction")]
+	static class PawnBlockConstruction
+	{
+		static bool Prefix(ref bool __result, Thing t)
+		{
+			if (t is Pawn)
+			{
+				__result = false;
+				return false;
+			}
+			return true;
+		}
+	}
+}
