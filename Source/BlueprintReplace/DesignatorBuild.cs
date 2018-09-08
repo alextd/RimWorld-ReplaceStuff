4cbf90e06875086cda4a09f957a08c1334298cc4
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2018-09-08 04:29:33 -0700

    Blueprints wipe blueprints

---
+++ Source/BlueprintReplace/DesignatorBuild.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+using Harmony;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace Replace_Stuff.BlueprintReplace
+{
+	[HarmonyPatch(typeof(GenSpawn), "SpawningWipes")]
+	class WipeBlueprints
+	{
+		//public static bool SpawningWipes(BuildableDef newEntDef, BuildableDef oldEntDef)
+		public static void Postfix(BuildableDef newEntDef, BuildableDef oldEntDef, ref bool __result)
+		{
+			if (__result) return;
+
+			if (newEntDef is ThingDef newD && newD.IsBlueprint &&
+					oldEntDef is ThingDef oldD && oldD.IsBlueprint)
+				__result = true;
+		}
+	}
+}
