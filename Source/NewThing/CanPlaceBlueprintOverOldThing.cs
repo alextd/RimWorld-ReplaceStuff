c972b5d8e8c21e57ef1b4aa371ad031ce820f6d2
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2018-09-24 15:37:36 -0700

    Make sure blueprints can be placed if new thing can replace old thing

---
+++ Source/NewThing/CanPlaceBlueprintOverOldThing.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using RimWorld;
+using Harmony;
+
+namespace Replace_Stuff.NewThing
+{
+	[HarmonyPatch(typeof(GenConstruct), "CanPlaceBlueprintOver")]
+	class CanPlaceBlueprintOverOldThing
+	{
+		//public static bool CanPlaceBlueprintOver(BuildableDef newDef, ThingDef oldDef)
+		public static void Postfix(ref bool __result, BuildableDef newDef, ThingDef oldDef)
+		{
+			if (__result) return;
+
+			if(newDef is ThingDef newD && newD.CanReplace(oldDef))
+				__result = true;
+		}
+	}
+}
