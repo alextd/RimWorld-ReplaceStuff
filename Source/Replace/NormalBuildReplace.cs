1ef77701cb4e85e32635fa100e91fd7cb88d6165
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2018-11-01 20:51:43 -0700

    ReplaceStuff Doors can be placed over doors with different rotation, since door rotation is automated once built so rotation doesn't matter when placing

--- Source/Replace/NormalBuildReplace.cs
+++ Source/Replace/NormalBuildReplace.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Reflection;
+using System.Reflection.Emit;
 using Harmony;
 using RimWorld;
 using Verse;
@@ -37,7 +39,8 @@ namespace Replace_Stuff
 			ThingDef newStuff = Designator_Build_Stuff.stuffDef;
 
 			foreach (Thing thing in center.GetThingList(map))
-				if (thing != thingToIgnore && thing.Position == center && thing.Rotation == rot &&
+				if (thing != thingToIgnore && thing.Position == center &&
+					(thing.Rotation == rot || PlacingRotationDoesntMatter(entDef)) &&
 					GenConstruct.BuiltDefOf(thing.def) == entDef)
 				{
 					ThingDef oldStuff = thing is Blueprint bp ? bp.UIStuff() : thing.Stuff;
@@ -49,6 +52,32 @@ namespace Replace_Stuff
 					if (newStuff != oldStuff)
 						__result = true;
 				}
+		}
+
+		public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+		{
+			MethodInfo RotationEquals = AccessTools.Method(typeof(Rot4), "op_Equality");
+			MethodInfo OrRotDoesntMatter = AccessTools.Method(typeof(NormalBuildReplace), nameof(OrRotDoesntMatter));
+
+			foreach (CodeInstruction i in instructions)
+			{
+				yield return i;
+				if (i.operand == RotationEquals)
+				{
+					yield return new CodeInstruction(OpCodes.Ldarg_0);//BuildableDef entDef
+					yield return new CodeInstruction(OpCodes.Call, OrRotDoesntMatter);
+				}
+			}
+		}
+
+		public static bool OrRotDoesntMatter(bool result, BuildableDef entDef)
+		{
+			return result || PlacingRotationDoesntMatter(entDef);
+		}
+
+		public static bool PlacingRotationDoesntMatter(BuildableDef entDef)
+		{
+			return (entDef is ThingDef def && def.thingClass == typeof(Building_Door));
 		}
 	}
 }
