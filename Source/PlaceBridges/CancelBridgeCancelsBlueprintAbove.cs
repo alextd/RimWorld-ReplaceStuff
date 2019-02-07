810db43a39a6af3efe217f0ea114a61f8fb2ecfa
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2019-02-07 12:24:54 -0800

    removing above canceled bridge frames, too, giving refund on frames too.

--- Source/PlaceBridges/CancelBridgeCancelsBlueprintAbove.cs
+++ Source/PlaceBridges/CancelBridgeCancelsBlueprintAbove.cs
@@ -8,19 +8,44 @@ using Harmony;
 
 namespace Replace_Stuff.PlaceBridges
 {
-	[HarmonyPatch(typeof(Blueprint), nameof(Blueprint.DeSpawn))]
-	class CancelBridgeCancelsBlueprintAbove
+	public static class CancelAboveBridges
 	{
-		//public override void DeSpawn(DestroyMode mode = DestroyMode.Vanish)
-		public static void Prefix(Blueprint __instance, DestroyMode mode)
+		public static void CancelAbove(BuildableDef defToBuild, DestroyMode mode, Map map, IntVec3 pos)
 		{
-			if (__instance.def.entityDefToBuild == TerrainDefOf.Bridge 
+			if (defToBuild == TerrainDefOf.Bridge
 				&& mode != DestroyMode.Vanish)
 			{
-				__instance.Map.thingGrid.ThingsListAtFast(__instance.Position)
-					.Where(t => t is Blueprint && t != __instance).ToList()
-					.Do(t => t.Destroy());
+				List<Thing> toKill = new List<Thing>();
+				foreach(Thing thing in map.thingGrid.ThingsListAtFast(pos))
+				{
+					if (thing is Blueprint bp && bp.def.entityDefToBuild != TerrainDefOf.Bridge)
+						toKill.Add(thing);
+					if (thing is Frame fr && fr.def.entityDefToBuild != TerrainDefOf.Bridge)
+						toKill.Add(thing);
+				}
+				toKill.Do(t => t.Destroy(DestroyMode.Refund));
 			}
 		}
 	}
+
+	[HarmonyPatch(typeof(Blueprint), nameof(Blueprint.DeSpawn))]
+	public static class CancelBlueprint
+	{ 
+		//public override void DeSpawn(DestroyMode mode = DestroyMode.Vanish)
+		public static void Prefix(Blueprint __instance, DestroyMode mode)
+		{
+			CancelAboveBridges.CancelAbove(__instance.def.entityDefToBuild, mode, __instance.Map, __instance.Position);
+		}
+	}
+
+	[HarmonyPatch(typeof(Frame), nameof(Frame.Destroy))]
+	public static class CancelFrame
+	{
+		//publicpublic override void Destroy(DestroyMode mode = DestroyMode.Vanish)
+		public static void Prefix(Frame __instance, DestroyMode mode)
+		{
+			CancelAboveBridges.CancelAbove(__instance.def.entityDefToBuild, mode, __instance.Map, __instance.Position);
+		}
+	}
+	
 }
