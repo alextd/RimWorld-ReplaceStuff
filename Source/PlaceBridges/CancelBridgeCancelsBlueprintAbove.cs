8a4a7a0cb1d3b96f4298663250d035cfeb77df46
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2019-02-07 12:01:20 -0800

    Canceling bridge blueprint cancels the above blueprint that no longer can be built.

---
+++ Source/PlaceBridges/CancelBridgeCancelsBlueprintAbove.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using RimWorld;
+using Harmony;
+
+namespace Replace_Stuff.PlaceBridges
+{
+	[HarmonyPatch(typeof(Blueprint), nameof(Blueprint.DeSpawn))]
+	class CancelBridgeCancelsBlueprintAbove
+	{
+		//public override void DeSpawn(DestroyMode mode = DestroyMode.Vanish)
+		public static void Prefix(Blueprint __instance, DestroyMode mode)
+		{
+			if (__instance.def.entityDefToBuild == TerrainDefOf.Bridge 
+				&& mode != DestroyMode.Vanish)
+			{
+				__instance.Map.thingGrid.ThingsListAtFast(__instance.Position)
+					.Where(t => t is Blueprint && t != __instance).ToList()
+					.Do(t => t.Destroy());
+			}
+		}
+	}
+}
