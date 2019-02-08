2aeb750b0588128051ed958b6bfb6f608732116d
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2019-02-08 04:59:23 -0800

    Destroyed bridges also remove blueprints above it

--- Source/PlaceBridges/CancelBridgeCancelsBlueprintAbove.cs
+++ Source/PlaceBridges/CancelBridgeCancelsBlueprintAbove.cs
@@ -13,11 +13,12 @@ namespace Replace_Stuff.PlaceBridges
 		public static void CancelAbove(BuildableDef defToBuild, DestroyMode mode, Map map, IntVec3 pos)
 		{
 			if (defToBuild == TerrainDefOf.Bridge
-				&& !(mode == DestroyMode.Vanish || mode == DestroyMode.FailConstruction))
+				&& mode != DestroyMode.Vanish && mode != DestroyMode.FailConstruction)
 			{
 				List<Thing> toKill = new List<Thing>();
 				foreach(Thing thing in map.thingGrid.ThingsListAtFast(pos))
 				{
+					//this sorta assumes the thing is not actually built, vanilla would handle that.
 					if (thing is Blueprint bp && bp.def.entityDefToBuild != TerrainDefOf.Bridge)
 						toKill.Add(thing);
 					if (thing is Frame fr && fr.def.entityDefToBuild != TerrainDefOf.Bridge)
@@ -49,5 +50,14 @@ namespace Replace_Stuff.PlaceBridges
 			CancelAboveBridges.CancelAbove(__instance.def.entityDefToBuild, mode, __instance.Map, __instance.Position);
 		}
 	}
-	
+
+	[HarmonyPatch(typeof(TerrainGrid), nameof(TerrainGrid.RemoveTopLayer))]
+	public static class DestroyedTerrain
+	{
+		//public void RemoveTopLayer(IntVec3 c, bool doLeavings = true)
+		public static void Prefix(TerrainGrid __instance, IntVec3 c, Map ___map)
+		{
+			CancelAboveBridges.CancelAbove(__instance.TerrainAt(c), DestroyMode.KillFinalize, ___map, c);
+		}
+	}
 }
