4f5405f0a1b91e8aa279a28c80f31e6297b14737
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2019-03-09 19:42:02 -0800

    Save destroyed building and re-spawn them when rebuilt; todo: MapComponent, clear saved buildings when blueprint dies

---
+++ Source/DestroyedRestore/DestroyedBuildings.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.Reflection.Emit;
+using Verse;
+using RimWorld;
+using Harmony;
+
+namespace Replace_Stuff.DestroyedRestore
+{
+	[HarmonyPatch(typeof(ThingUtility), nameof(ThingUtility.CheckAutoRebuildOnDestroyed))]
+	static class DestroyedBuildings
+	{
+		public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+		{
+			MethodInfo PlaceBlueprintForBuildInfo = AccessTools.Method(typeof(GenConstruct), nameof(GenConstruct.PlaceBlueprintForBuild));
+
+			foreach (CodeInstruction i in instructions)
+			{
+				yield return i;
+				if(i.opcode == OpCodes.Call && i.operand == PlaceBlueprintForBuildInfo)
+				{
+					yield return new CodeInstruction(OpCodes.Ldarg_0);//Thing thing
+					yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(DestroyedBuildings), nameof(SaveBuilding)));//SaveBuilding(thing)
+				}
+			}
+		}
+
+		//todo: more maps
+		public static Dictionary<IntVec3, Thing> destroyedBuildings = new Dictionary<IntVec3, Thing>();
+		public static void SaveBuilding(Thing thing)
+		{
+			Log.Message($"Saving {thing} to {thing.Position}");
+			destroyedBuildings[thing.Position] = thing;
+			thing.ForceSetStateToUnspawned();
+		}
+	}
+}
---
+++ Source/DestroyedRestore/ReviveBuilding.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.Reflection.Emit;
+using Verse;
+using RimWorld;
+using Harmony;
+
+namespace Replace_Stuff.DestroyedRestore
+{
+	[HarmonyPatch(typeof(Frame), nameof(Frame.CompleteConstruction))]
+	static class ReviveBuilding
+	{
+		//public void CompleteConstruction(Pawn worker)
+		public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+		{
+			//ThingMaker.MakeThing(thingDef, base.Stuff);
+			MethodInfo MakeThingInfo = AccessTools.Method(typeof(ThingMaker), nameof(ThingMaker.MakeThing));
+			MethodInfo CheckForRevivalInfo = AccessTools.Method(typeof(ReviveBuilding), nameof(CheckForRevival));
+
+			foreach(CodeInstruction i in instructions)
+			{
+				if(i.opcode == OpCodes.Call && i.operand == MakeThingInfo)
+				{
+					yield return new CodeInstruction(OpCodes.Ldarg_0);//Frame
+					yield return new CodeInstruction(OpCodes.Call, CheckForRevivalInfo);//CheckForRevival(thingDef, base.Stuff, Frame)
+				}
+				else 
+					yield return i;
+			}
+		}
+
+		//public static Thing MakeThing(ThingDef def, ThingDef stuff = null)
+		public static Thing CheckForRevival(ThingDef def, ThingDef stuff, Frame frame)
+		{
+			Log.Message($"Finding for {frame} at {frame.Position}");
+			if(DestroyedBuildings.destroyedBuildings.TryGetValue(frame.Position, out Thing building))
+			{
+				Log.Message($"got {building}");
+				building.stackCount = 1;
+				DestroyedBuildings.destroyedBuildings.Remove(frame.Position);
+
+				return building;
+			}
+			return ThingMaker.MakeThing(def, stuff);
+		}
+	}
+}
