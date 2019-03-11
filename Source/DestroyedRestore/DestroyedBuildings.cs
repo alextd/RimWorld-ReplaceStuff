03622ed5860e6918dbec1d3e8a0c82807fb4dcf7
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2019-03-10 18:14:41 -0700

    extracting BuildingReviver.cs to its own file to find it easier

--- Source/DestroyedRestore/DestroyedBuildings.cs
+++ Source/DestroyedRestore/DestroyedBuildings.cs
@@ -45,7 +45,7 @@ namespace Replace_Stuff.DestroyedRestore
 		{
 			Scribe_Collections.Look(ref destroyedBuildings, "destroyedBuildings", LookMode.Value, LookMode.Deep);
 		}
-		
+
 
 		public static void SaveBuilding(Thing thing, Map map)
 		{
@@ -78,62 +78,6 @@ namespace Replace_Stuff.DestroyedRestore
 				//Probably should set building.mapIndexOrState to -2
 				comp.destroyedBuildings.Remove(pos);
 			}
-		}
-	}
-
-	[StaticConstructorOnStartup]
-	public static class BuildingReviver
-	{
-		public static Dictionary<Type, Action<Thing, Thing>> handlers;
-		static BuildingReviver()
-		{
-			handlers = new Dictionary<Type, Action<Thing, Thing>>();
-
-			//Here are the types 
-			handlers[typeof(Building_WorkTable)] = delegate (Thing fromThing, Thing toThing)
-			{
-				if (fromThing is Building_WorkTable from && toThing is Building_WorkTable to)
-					foreach (Bill bill in from.BillStack)
-						to.BillStack.AddBill(bill);
-			};
-			handlers[typeof(Building_Cooler)] = delegate (Thing fromThing, Thing toThing)
-			{
-				if (fromThing is Building_Cooler from && toThing is Building_Cooler to)
-					to.compTempControl.targetTemperature = from.compTempControl.targetTemperature;
-			};
-			handlers[typeof(Building_Heater)] = delegate (Thing fromThing, Thing toThing)
-			{
-				if (fromThing is Building_Heater from && toThing is Building_Heater to)
-					to.compTempControl.targetTemperature = from.compTempControl.targetTemperature;
-			};
-			handlers[typeof(Building_PlantGrower)] = delegate (Thing fromThing, Thing toThing)
-			{
-				if (fromThing is Building_PlantGrower from && toThing is Building_PlantGrower to)
-					to.SetPlantDefToGrow(from.GetPlantDefToGrow());
-			};
-			handlers[typeof(Building_Storage)] = delegate (Thing fromThing, Thing toThing)
-			{
-				if (fromThing is Building_Storage from && toThing is Building_Storage to)
-					to.settings.CopyFrom(from.GetStoreSettings());
-			};
-			//If building_bed didn't forget their owner it'd be easier;
-			//otherwise the entire system could save an object instead of the Building before it despawns. 
-
-			//CompProperties_Refuelable if targetFuelLevelConfigurable
-		}
-
-		public static bool CanDo(Thing thing)
-		{
-			return handlers.ContainsKey(thing.GetType());
-		}
-
-		public static void Transfer(Thing from, Thing to)
-		{
-			if (handlers.TryGetValue(from.GetType(), out Action<Thing, Thing> handler))
-			{
-				handler(from, to);
-			}
-			//else log warning no this shouldn't happen
 		}
 	}
 }
