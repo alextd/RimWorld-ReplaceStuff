d964241469624cf1b4358af95e25a8d8bb60ecce
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2020-03-02 23:33:51 -0800

    Make sure modded stuffed coolers + 2-wide overwall coolers don't conflict with two dropdown menus

--- Source/Replace_Stuff.cs
+++ Source/Replace_Stuff.cs
@@ -27,6 +27,7 @@ namespace Replace_Stuff
 				//Hugslibs-added defs will be queued up before this Static Constructor
 				//So queue replace frame generation after that
 				LongEventHandler.QueueLongEvent(() => ThingDefGenerator_ReplaceFrame.AddReplaceFrames(), null, true, null);
+				LongEventHandler.QueueLongEvent(() => CoolersOverWalls.DesignatorBuildDropdownStuffFix.SanityCheck(), null, true, null);
 			}
 		}
 
