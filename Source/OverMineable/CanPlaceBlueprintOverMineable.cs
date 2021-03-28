825e5d583df32eedf826948c764e87610de1ac3e
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2021-03-28 13:06:06 -0700

    Should fix RimFactory conflict (mineshaft which can be built over rock using its PlaceWorker)

--- Source/OverMineable/CanPlaceBlueprintOverMineable.cs
+++ Source/OverMineable/CanPlaceBlueprintOverMineable.cs
@@ -16,6 +16,12 @@ namespace Replace_Stuff.OverMineable
 		{
 			return td.mineable && !td.IsSmoothed;
 		}
+		public static bool IsBlockingRock(this Thing t, Thing placedThing) => IsBlockingRock(t.def, placedThing.def);
+		public static bool IsBlockingRock(this ThingDef td, BuildableDef placingDef)
+		{
+			//This checks ForceAllow, but not AllowsPlacing, since AllowsPlacing defaults to true, and PlaceWorks like ShowFacilites would be true.
+			return td.IsMineableRock() && !placingDef.ForceAllowPlaceOver(td);
+		}
 	}
 	
 	[HarmonyPatch(typeof(GenConstruct), "CanPlaceBlueprintOver")]
@@ -42,7 +48,7 @@ namespace Replace_Stuff.OverMineable
 		{
 			if (__result) return;
 			
-			if (t.IsMineableRock())	// any case that the thing can be built over plain rock?
+			if (t.IsBlockingRock(constructible))
 				__result = true;
 		}
 	}
@@ -68,7 +74,7 @@ namespace Replace_Stuff.OverMineable
 					continue;
 
 				if (sourceDef is ThingDef thingDef)
-					foreach (Thing mineThing in map.thingGrid.ThingsAt(cell).Where(t => t.IsMineableRock()))
+					foreach (Thing mineThing in map.thingGrid.ThingsAt(cell).Where(t => t.def.IsBlockingRock(sourceDef)))
 					{
 						if (!DontMineSmoothingRock.ToBeSmoothed(mineThing, thingDef))
 						{
