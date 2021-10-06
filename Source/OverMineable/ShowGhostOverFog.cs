beae3df21016260e05579cb7fd2f94b7ca3078d4
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2021-10-06 16:30:27 -0700

    Fix Graphic_Appearances to pass along the GraphicData to the GraphicDatabase, so that it at least uses the given renderQueue.
    All so that Fence blueprints can render over fog (and over walls, I guess!) given a higher renderQueue. Since fence blueprints seem to be the only thing with a blueprintGraphicData that uses Graphic_Appearances

--- Source/OverMineable/ShowGhostOverFog.cs
+++ Source/OverMineable/ShowGhostOverFog.cs
@@ -32,7 +32,8 @@ namespace Replace_Stuff.OverMineable
 				HarmonyLib.Transpilers.MethodReplacer(instructions, GraphicGetInfo, GraphicGetRenderHighInfo)
 				, CopyFromInfo, CopyFromRenderHighInfo);
 		}
-		//		public void CopyFrom(GraphicData other)
+
+		//public void CopyFrom(GraphicData other)
 		public static void CopyFromRenderHigh(GraphicData instance, GraphicData other)
 		{
 			instance.CopyFrom(other);
@@ -60,4 +61,50 @@ namespace Replace_Stuff.OverMineable
 			__result.graphicData.linkFlags &= ~LinkFlags.Rock;//Prevent blueprint walls from showing links with rocks
 		}
 	}
+
+	//Graphic_Appearances doesn't pass renderQueue along.
+	//Fences blueprints uses Graphic_Appearances and nothing else does.
+	//Fix that bug so fences blueprints can render over fog
+
+	[HarmonyPatch(typeof(Graphic_Appearances), nameof(Graphic_Appearances.Init))]
+	public static class GraphicAppearancesPassData
+	{
+		//public override void Init(GraphicRequest req)
+		public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+		{
+			//public static Graphic Get<T>(string path, Shader shader, Vector2 drawSize, Color color) where T : Graphic, new()
+			//public static Graphic Get<T>(string path, Shader shader, Vector2 drawSize, Color color, Color colorTwo, GraphicData data, string maskPath = null) where T : Graphic, new()
+
+			MethodInfo GetInfo = AccessTools.Method(typeof(GraphicDatabase), nameof(GraphicDatabase.Get),
+					parameters: new Type[] { typeof(string), typeof(Shader), typeof(Vector2), typeof(Color) },
+					generics: new Type[] { typeof(Graphic_Single) });
+			MethodInfo GetWithDataInfo = AccessTools.Method(typeof(GraphicDatabase), nameof(GraphicDatabase.Get),
+					parameters: new Type[] { typeof(string), typeof(Shader), typeof(Vector2), typeof(Color), typeof(Color), typeof(GraphicData), typeof(string) },
+					generics: new Type[] { typeof(Graphic_Single) });
+
+			//Color.white
+			MethodInfo ColorWhiteInfo = AccessTools.Property(typeof(Color), nameof(Color.white)).GetGetMethod();
+
+			//Graphic.data
+			FieldInfo dataInfo = AccessTools.Field(typeof(Graphic), nameof(Graphic.data));
+
+			foreach (var inst in instructions)
+			{
+				if(inst.Calls(GetInfo))
+				{
+					//From:	GraphicDatabase.Get<Graphic_Single>(text + "/" + texture2D.name, req.shader, drawSize, color);
+					//To:		GraphicDatabase.Get<Graphic_Single>(text + "/" + texture2D.name, req.shader, drawSize, color, Color.white, this.data, null);
+					yield return new CodeInstruction(OpCodes.Call, ColorWhiteInfo);//Color.white (property getter)
+					yield return new CodeInstruction(OpCodes.Ldarg_0);//this(Graphic_Appearances)
+					yield return new CodeInstruction(OpCodes.Ldfld, dataInfo);//this.data(GraphicData)
+					yield return new CodeInstruction(OpCodes.Ldnull);//null (string)
+					inst.operand = GetWithDataInfo;//Override method...
+					yield return inst;//Get(path, shader, drawSize, color, Color.white, this.data, null);
+				}
+				else
+					yield return inst;
+			}
+		}
+	}
+
 }
