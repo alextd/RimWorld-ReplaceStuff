c0680533d94c568593fa322cc104c8cfddcd3da4
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2022-02-18 00:30:13 -0800

    Save/Load the order of bridges to use

--- Source/Settings.cs
+++ Source/Settings.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using System.Reflection.Emit;
 using UnityEngine;
@@ -46,11 +47,39 @@ namespace Replace_Stuff
 
 			options.End();
 		}
-		
+
 		public override void ExposeData()
 		{
 			Scribe_Values.Look(ref hideOverwallCoolers, "hideOverwallCoolers", false);
 			Scribe_Values.Look(ref hideNormalCoolers, "hideNormalCoolers", false);
+
+			if (Scribe.mode == LoadSaveMode.Saving)
+			{
+				Scribe_Collections.Look(ref BridgelikeTerrain.allBridgeTerrains, "bridgePrefNames");
+			}
+			else if (Scribe.mode == LoadSaveMode.LoadingVars)
+			{
+				List<string> defNames = null;
+				Scribe_Collections.Look(ref defNames, "bridgePrefNames");
+
+				//Gotta wait for DefOfs to load to use DefDatabase
+				LongEventHandler.ExecuteWhenFinished(() =>
+				{
+					List<TerrainDef> loadedBridgeOrder = defNames.Select(n => DefDatabase<TerrainDef>.GetNamed(n, false)).ToList();
+					loadedBridgeOrder.RemoveAll(d => d == null);//Any removed mods, forget about em.
+
+					//To merge with maybe new modded bridges:
+					//Take all from known loadedBridgeOrder and push to front:
+					//Any new modded terrains will be in back - which is normal for modded terrain anyway.
+					//TODO: order default list by cost or something. Meh.
+					loadedBridgeOrder.Reverse();
+					foreach (TerrainDef terDef in loadedBridgeOrder)
+					{
+						BridgelikeTerrain.allBridgeTerrains.Remove(terDef);
+						BridgelikeTerrain.allBridgeTerrains.Insert(0, terDef);
+					}
+				});
+			}
 		}
 	}
 
