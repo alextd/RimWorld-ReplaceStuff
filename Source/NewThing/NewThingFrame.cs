97b15cce81eafd596e2f0f7bdbc0c07798969af7
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2024-05-15 10:09:42 -0700

    Check isPlaceOverableWall to determine isWall

--- Source/NewThing/NewThingFrame.cs
+++ Source/NewThing/NewThingFrame.cs
@@ -130,7 +130,7 @@ namespace Replace_Stuff.NewThing
 			//---------------------------------------------
 			//---------------------------------------------
 			//Here are valid replacements:
-			replacements.Add(new Replacement(d => d.IsWall() || (d.building?.isFence ?? false) || typeof(Building_Door).IsAssignableFrom(d.thingClass)));
+			replacements.Add(new Replacement(d => d.IsWall() || (d.building?.isPlaceOverableWall ?? false) || (d.building?.isFence ?? false) || typeof(Building_Door).IsAssignableFrom(d.thingClass)));
 			replacements.Add(new Replacement(d => typeof(Building_Cooler).IsAssignableFrom(d.thingClass),
 				postAction: (n, o) =>
 				{
